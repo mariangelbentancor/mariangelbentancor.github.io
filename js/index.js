8be8608d2e7deb308baf70f25fa90fa55bc1e41a
Mariángel Bentancor <izavobentancor@hotmail.com>
2022-08-16 22:33:41 -0300

    Punto 3 Formulario

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const login = document.getElementById('ingresar')
+
+    login.addEventListener('click', () =>  {
+        window.location.href = 'home.html'
+    })
+})
